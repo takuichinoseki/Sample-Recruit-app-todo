3ee2a584ebf487a4028c796eb3cc5cf002ae1d18
一関　拓 <t.ichinoseki.carecon@gmail.com>
2023-11-08 17:02:07 +0900

    マイグレーションファイル作成

--- database/migrations/2023_11_08_074801_create_jobs_table.php
+++ database/migrations/2023_11_08_074801_create_jobs_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('start_at');
+            $table->boolean('delete_flg');
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('jobs');
     }
 };
